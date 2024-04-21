44fb138df872f973bf45f7e35828d91c70bbe4cd
hamza <hamzadevops@hotmail.com>
2024-04-21 13:45:49 +0100

    ignore

--- cmd.sh
+++ cmd.sh
@@ -12,8 +12,20 @@ git push origin develop --force
 
 
 ==================
-git checkout -b feature/FA-40-unknown-activity-display-transactions-logic
 
 git checkout develop
+
+git pull origin develop
+
+git checkout -b feature/FA-40-unknown-activity-display-transactions-logic
+
+git push -u origin feature/FA-40-unknown-activity-display-transactions-logic
+
+git branch --show-current
+===========
+git checkout develop
+git pull origin develop
+git merge feature/FA-81-Create-custom-component-search
+
 
 
